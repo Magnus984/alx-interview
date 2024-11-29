828e13aa05bf1d2fd8e0eb46aab1fdab98a7b247
Magnus <nharnharyhaw984@gmail.com>
2024-11-29 17:39:14 +0000

    added files

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Minimum operations
+"""
+
+def minOperations(n):
+    """Calculate the minimum number of operations required
+    to achieve exactly n 'H' characters.
+    """
+    if not isinstance(n, int):
+        return 0
+    operations_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            operations_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            operations_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            operations_count += 1
+    return operations_count
+    
